08e4db2d3ecc1f3da7dd0eb949f6cc43afd79b5a
shubhangi-singh21 <shubhangifas@gmail.com>
2020-11-01 14:25:52 +0530

    feat(main):simultaneous functioning of dices

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -34,6 +34,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(() {
                   leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -43,6 +44,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
                   rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
