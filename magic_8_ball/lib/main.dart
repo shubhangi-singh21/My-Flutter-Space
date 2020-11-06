09c81c142d7f021d0b0a53d422d627da0258a0ab
shubhangi-singh21 <shubhangifas@gmail.com>
2020-11-07 00:56:23 +0530

    feat(main);appBar

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Center(child: Text('Ask Me Anything')),
       ),
-    );
+      body: Column(),
+    ));
+  }
+}
