a7aaa35d55964fb35a019528fc83a64ced09d64f
shubhangi-singh21 <shubhangifas@gmail.com>
2020-11-26 00:54:27 +0530

    feat(main):added stateful widget Ball

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -4,16 +4,31 @@ void main() {
   runApp(MyApp());
 }
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('image/ball1.png'),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      backgroundColor: Colors.blueAccent,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.indigo,
         title: Center(child: Text('Ask Me Anything')),
       ),
-      body: Column(),
+      body: Ball(),
     ));
   }
 }
