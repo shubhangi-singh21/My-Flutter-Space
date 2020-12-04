243b0f571084ca511488a6036f057c92433aa6ba
shubhangi-singh21 <shubhangifas@gmail.com>
2020-12-04 23:53:43 +0530

    feat(main);added multiple buttons🎹

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -10,12 +10,61 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
             child: Center(
-                child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note1.wav');
-                    },
-                    child: Text('Click Here')))),
+                child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.orange,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note2.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.yellow,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note3.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note4.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.teal,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note5.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.blueAccent,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note6.wav');
+              },
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note7.wav');
+              },
+            ),
+          ],
+        ))),
       ),
     );
   }
